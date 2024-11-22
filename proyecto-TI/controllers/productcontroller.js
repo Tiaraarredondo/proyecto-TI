f8267e956d6333e630ef6968395cfebee18c0bc4
sofiagarziaa <sgarzia@udesa.edu.ar>
2024-11-22 17:53:22 -0300

    controlador producto

--- proyecto-TI/controllers/productcontroller.js
+++ proyecto-TI/controllers/productcontroller.js
@@ -1,62 +1,48 @@
-/*const db = require('../database/models');
+const db = require('../database/models');
 const op = db.Sequelize.Op;
 
 const productController = {
-
-  // Listar todos los productos
-  list: function(req, res) {
-    db.Product.findAll()
+    // Mostrar todos los productos
+    list: (req, res) => {
+        db.Products.findAll()
+            .then(products => {
+                res.render('products', { products }); // Renderiza la vista 'products.ejs' con los productos
+            })
+            .catch(err => {
+                console.log(err);
+                
+            });
+    },
+    detail: function(req, res) {
+      const { id } = req.params;
+      db.Products.findByPk(id)
+          .then(product => {
+              if (product) {
+                  res.render('products/detail', { product }); // Renderiza la vista con el producto
+              } else {
+                  res.status(404).send('Producto no encontrado'); // Producto no encontrado
+              }
+          })
+          .catch(error => {
+              console.log(error); // Registra errores inesperados
+          });
+  },
+  
+    // Mostrar el formulario para crear un nuevo producto
+    new: function(req, res) {
+      db.Products.findAll({
+          order: [['created_at', 'DESC']], // Ordena por fecha de creación descendente
+          limit: 5 // Limita el resultado a los últimos 5 productos
+      })
       .then(products => {
-        res.render('products/list', { products });
+          res.render('products/new', { products }); // Renderiza una vista para mostrar los productos
       })
       .catch(error => {
-        console.log(error);
-        
+          console.log(error); // Maneja errores
+          res.status(500).send('Error al listar los productos más recientes'); // Responde con un error si algo falla
       });
-  },
-
-  // Detalle de un producto específico
-  detail: function(req, res) {
-    const { id } = req.params;
-    db.Product.findByPk(id)
-      .then(product => {
-        if (product) {
-          res.render('product/detail', { product });
-        } else {
-          res.status(404).send('Producto no encontrado');
-        }
-      })
-      .catch(error => {
-        console.log(error);
-      });
-  },
-
-  // Últimos 5 productos 
-  new: function(req, res) {
-    db.Product.findAll({
-      order: [['release_date', 'DESC']],
-      limit: 5
-    })
-    .then(products => {
-      res.render('products/new', { products });
-    })
-    .catch(error => {
-        console.log(error);
-    });
-  },
-
-  // Películas recomendadas (rating >= 8)
-  recommended: function(req, res) {
-    db.Product.findAll({
-      where: { rating: { [op.gte]: 8 } }
-    })
-    .then(products => {
-      res.render('products/recommended', { products });
-    })
-    .catch(error => {
-        console.log(error);
-    });
   }
+  
 };
 
-module.exports = productController;*/
+module.exports = productController;
